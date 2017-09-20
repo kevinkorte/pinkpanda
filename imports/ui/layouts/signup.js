d18a1ba1c23d00ae41b9011b5ea8e846c13a2f25
kevinkorte <kevinkorte15@gmail.com>
2017-09-19 21:33:35 -0700

    started new user sign up - TODO: work on server side next

--- imports/ui/layouts/signup.js
+++ imports/ui/layouts/signup.js
@@ -1,1 +1,20 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
 import './signup.html';
+
+Template.App_signup.events({
+  'submit .js-submit'(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    Meteor.call('newUserSignup', email, password, (error, response) => {
+      if ( error ) {
+        //Todo: Better error handling
+        console.log(error)
+      } else {
+        //Successfully logged in, go to the next route
+      }
+    })
+  }
+})
