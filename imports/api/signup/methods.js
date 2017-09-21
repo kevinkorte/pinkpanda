7c221b4d4d337f5ff4a90d8cc06b4008a293eece
kevinkorte <kevinkorte15@gmail.com>
2017-09-20 22:55:47 -0700

    ready to update user account with stripe customer id

--- imports/api/signup/methods.js
+++ imports/api/signup/methods.js
@@ -9,16 +9,23 @@ const stripe = stripePackage(Meteor.settings.private.stripeSecretKey);
 
 Meteor.methods({
   newUserSignup(email, password) {
-    //Todo: Check if user email already exists in the database
-    let asyncToSync = Meteor.wrapAsync( stripe.customers.create, stripe.customers );
-    resultOfStripeCreateCustomer = asyncToSync( { email: email } );
-    console.log( resultOfStripeCreateCustomer );
-    _createUserAccount( resultOfStripeCreateCustomer, password );
-
+    let checkIfUserExists = Accounts.findUserByEmail( email );
+    if ( !checkIfUserExists ) {
+      let asyncToSync = Meteor.wrapAsync( stripe.customers.create, stripe.customers );
+      resultOfStripeCreateCustomer = asyncToSync( { email: email } );
+      let userId = _createUserAccount( resultOfStripeCreateCustomer, password );
+                   _updateUserAccount( resultOfStripeCreateCustomer, userId );
+    } else {
+      throw new Meteor.Error('new-user-already-exists', "Email already exists.");
+    }
   },
 });
 
 let _createUserAccount = ( customer, password ) => {
   let userId = Accounts.createUser( { email: customer.email, password: password } );
-  console.log('user id', userId);
+  return userId;
+};
+let _updateUserAccount = ( customer, userId ) => {
+  //Todo Attach Stripe customer id to user accounts id
+  console.log(userId, 'update user Accounts', customer);
 }
