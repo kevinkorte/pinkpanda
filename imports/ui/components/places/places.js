2f226bf5c74c81ace89519b5ccafad2e8015eeea
kevinkorte <kevinkorte15@gmail.com>
2017-10-15 21:37:54 -0700

    night push

--- imports/ui/components/places/places.js
+++ imports/ui/components/places/places.js
@@ -13,9 +13,25 @@ Template.places.onRendered( function() {
   autocomplete = new google.maps.places.Autocomplete(input);
   console.log(autocomplete);
   autocomplete.addListener('place_changed', function() {
-    console.log('event listener');
     let place = autocomplete.getPlace();
     Session.set('place', place);
+    Session.set('place_photo', place.photos[0].getUrl({maxWidth: 640}))
+    console.log(place.photos[0].getUrl({maxWidth: 640}));
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 17
+    });
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': place.formatted_address}, function( results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status)
+      }
+    })
   })
 });
 
@@ -24,6 +40,14 @@ Template.places.helpers({
     let place = Session.get('place');
     if ( place ) {
       return place;
+    } else {
+      return;
+    }
+  },
+  getPlacePhoto() {
+    let photo = Session.get('place_photo');
+    if ( photo ) {
+      return photo;
     } else {
       return;
     }
