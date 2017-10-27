27eba5548ef2a3283da67993f75e7472276f36da
kevinkorte <kevinkorte15@gmail.com>
2017-10-26 21:13:51 -0700

    can update or change your email address

--- imports/api/settings/methods.js
+++ imports/api/settings/methods.js
@@ -4,5 +4,14 @@ import { Accounts } from 'meteor/accounts-base';
 Meteor.methods({
   send_verification_email() {
     Accounts.sendVerificationEmail(Meteor.userId());
+  },
+  update_email( email ) {
+    let user = Meteor.users.findOne(Meteor.userId());
+    if ( user ) {
+      let userEmail = user.emails[0].address;
+      Accounts.addEmail(Meteor.userId(), email );
+      Accounts.sendVerificationEmail(Meteor.userId(), email );
+      Accounts.removeEmail(Meteor.userId(), userEmail );
+    }
   }
 })
--- imports/ui/components/update_login/update_login.js
+++ imports/ui/components/update_login/update_login.js
@@ -1,1 +1,19 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
 import './update_login.html';
+
+Template.update_login.events({
+  'submit #update-email-form'(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    Meteor.call('update_email', email, (error, response) => {
+      if ( error ) {
+        Bert.alert( error.reason, 'danger', 'fixed-top', 'fa-frown-o' );
+      } else {
+        Bert.alert( 'Email updated.', 'success', 'fixed-top', 'fa-check' );
+        $('#inputNewEmail').val('');
+      }
+    })
+  }
+})
