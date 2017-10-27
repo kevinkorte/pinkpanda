0dbc481ffd5a87c2713027b522d82a669ae7146c
kevinkorte <kevinkorte15@gmail.com>
2017-10-26 21:22:34 -0700

    night push, almost have password reset form working

--- imports/ui/components/changePassword/changePassword.js
+++ imports/ui/components/changePassword/changePassword.js
@@ -1,1 +1,40 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
 import './changePassword.html';
+
+Template.changePassword.events({
+  'submit #update-password'(event) {
+    event.preventDefault();
+    const target = event.target;
+    const currentPassword = target.currentPassword.value;
+    const newPassword = target.newPassword.value;
+    const confirmPassword = target.confirmPassword.value;
+    if (newPassword !== confirmPassword ) {
+      Session.set('passwordsDontMatch', 'Passwords do not match');
+    } else {
+      Accounts.changePassword(currentPassword, newPassword, function(error) {
+        if (error) {
+          Session.set('passwordsDontMatch', null);
+          Session.set('changePasswordError', error.reason);
+        } else {
+          $('.js-change-password').addClass('loading');
+          Session.set('passwordsDontMatch', null);
+          Session.set('changePasswordError', null);
+          Meteor.call('sendChangedPasswordEmail', function(error) {
+            if (error) {
+              Bert.alert( error.reason, 'danger', 'fixed-top', 'fa-frown-o' );
+            } else {
+              $('.js-change-password').removeClass('loading');
+              $('#resetPasswordModal').modal('hide');
+              swal({
+                title: "Password Successfully Changed",
+                type: "success"
+              });
+            }
+          });
+        }
+      });
+    }
+  }
+})
