d53b015e29852df063ffeb01bac70e6e5df3226f
Kevin Korte <kevinkorte15@gmail.com>
2017-11-03 07:34:16 -0700

    add followers is broken

--- imports/api/followers/methods.js
+++ imports/api/followers/methods.js
@@ -29,4 +29,54 @@ Meteor.methods({
       }
     });
   },
+  addFollowers: function(followers, eventId) {
+    check(followers, [String]);
+    check(eventId, String)
+    followers.forEach(function(followerId) {
+      let follower = Followers.findOne(followerId);
+      let viewing = Viewings.findOne(eventId);
+      if (follower) {
+        Viewings.update(eventId, {$addToSet: {
+          followers: {
+            id: follower._id,
+            name: follower.name,
+            phoneNumber: follower.phoneNumber,
+            email: follower.email
+          }
+        }
+      }, function(error, result) {
+          if (error) {
+            console.log(error);
+          } else {
+            SSR.compileTemplate('htmlEmail', Assets.getText('add-follower-email.html'));
+            SSR.compileTemplate('textMessage', Assets.getText('add-follower-text.html'));
+            console.log(follower.name);
+            let data = {
+              name: follower.name,
+              userName: Meteor.users.findOne(Meteor.userId())._id,
+              address: viewing.address,
+              url: 'http://localhost:3000/'+Meteor.userId()+'/'+viewing._id,
+            };
+            if (follower.email) {
+              Email.send({
+                to: follower.email,
+                from: "from.address@email.com",
+                subject: Meteor.users.findOne(Meteor.userId())._id+" has a new appointment",
+                html: SSR.render('htmlEmail', data),
+              });
+            }
+            if (follower.phoneNumber) {
+              client.messages.create({
+                  body: SSR.render('textMessage', data),
+                  to: '+1'+follower.phoneNumber,  // Text this number
+                  from: '+19417875497' // From a valid Twilio number
+              })
+              .then((message) => console.log(message.sid));
+            }
+          }
+        });
+      }
+    });
+    // Viewings.update(eventId, {$set: {followers.id: followers}})
+  }
 })
--- imports/ui/components/followers/followers.js
+++ imports/ui/components/followers/followers.js
@@ -18,4 +18,20 @@ Template.followers.helpers({
   follower() {
     return Followers.find();
   },
+});
+
+Template.followers.events({
+  'submit .add-followers'(event) {
+    event.preventDefault();
+    let followers = $('.edit-followers-input').val();
+    // followers.forEach(function(el) {console.log(el)});
+    let eventId = FlowRouter.getParam('id');
+    Meteor.call('addFollowers', followers, eventId, function(error, result) {
+      if (error) {
+        Bert.alert( error.reason, 'danger', 'fixed-top', 'fa-frown-o' );
+      } else {
+
+      }
+    })
+  },
 })
