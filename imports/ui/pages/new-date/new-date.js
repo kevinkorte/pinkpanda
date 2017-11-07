943b457bb2c02baadd6ed2c0526667ecee50dc74
kevinkorte <kevinkorte15@gmail.com>
2017-11-06 20:52:29 -0800

    working on saving all date info

--- imports/ui/pages/new-date/new-date.js
+++ imports/ui/pages/new-date/new-date.js
@@ -1,6 +1,22 @@
+import { Template } from 'meteor/templating';
 import './new-date.html';
 
 import '../../components/places/places.js';
 import '../../components/set-date/set-date.js';
 import '../../components/plus_one/plus_one.js';
 import '../../components/followers/followers.js';
+
+Template.new_date.events({
+  'submit #create-new-date'(event) {
+    event.preventDefault();
+    const target = event.target;
+    const start = target.startDate.value;
+    const end = target.endDate.value;
+    const place = target.place.value;
+    const lat = target.lat.value;
+    const lng = target.lng.value;
+    const dateName = target.dateName.value;
+    const dateURL = target.dateURL.value;
+    console.log(place);
+  }
+})
