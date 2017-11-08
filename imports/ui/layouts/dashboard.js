82339785c0742baf2c1e278cee079c86b5987ddc
kevinkorte <kevinkorte15@gmail.com>
2017-11-07 21:06:33 -0800

    meh

--- imports/ui/layouts/dashboard.js
+++ imports/ui/layouts/dashboard.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Dates } from '../../api/dates/dates.js';
 
+import moment from 'moment';
+
 import "./dashboard.html";
 import '../components/navigation.js';
 
@@ -17,5 +19,16 @@ Template.dashboard.helpers({
   dates() {
     let dates = Dates.find()
     return dates;
+  },
+  getStartHour(timestamp) {
+    return moment(timestamp).format('ha');
+  },
+  getStartDay(timestamp) {
+    let month = moment(timestamp).format('MMM');
+    let day = moment(timestamp).format('D');
+    return '<small>'+month+'</small><span class="font-weight-bold">'+day+'</span>';
+  },
+  getEndHour(timestamp) {
+    return moment(timestamp).format('ha');
   }
 })
