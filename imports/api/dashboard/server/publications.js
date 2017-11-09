f1c219b74984f9fe624d4433df0f4a320007f032
kevinkorte <kevinkorte15@gmail.com>
2017-11-08 21:36:35 -0800

    edit maps work, but marker is fixed position, need to fix

--- imports/api/dashboard/server/publications.js
+++ imports/api/dashboard/server/publications.js
@@ -4,3 +4,8 @@ import { Dates } from '../../dates/dates.js';
 Meteor.publish('dates.all', function () {
   return Dates.find();
 });
+
+Meteor.publish('dates.single', function (id) {
+  console.log(id);
+  return Dates.find({_id: id});
+})
--- imports/ui/pages/date_can_edit/date_can_edit.js
+++ imports/ui/pages/date_can_edit/date_can_edit.js
@@ -1,1 +1,29 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { Dates } from '../../../api/dates/dates.js';
+
 import './date_can_edit.html';
+
+Template.date_can_edit.onCreated(function() {
+  let self = this;
+  self.autorun(function() {
+    self.subscribe('dates.single', FlowRouter.getParam('id') );
+  });
+});
+
+Template.date_can_edit.onRendered(function() {
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17
+  });
+  map.setCenter({ lat: 46.2267042, lng:-119.24917970000001 });
+  let marker = new google.maps.Marker({
+    map: map,
+    position: { lat: 46.2267042, lng:-119.24917970000001 }
+  })
+})
+
+Template.date_can_edit.helpers({
+  date() {
+    return Dates.findOne();
+  }
+})
