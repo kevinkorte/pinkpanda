4f2e1d7da7b9165dabf85714f53aedbcb58953c7
kevinkorte <kevinkorte15@gmail.com>
2017-11-11 16:21:43 -0800

    added twilio account sid

--- imports/api/dates/methods.js
+++ imports/api/dates/methods.js
@@ -2,6 +2,12 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Dates } from './dates.js';
 
+import twilio from 'twilio';
+
+let accountSid = Meteor.settings.private.twilio.accountsid;
+let authToken = Meteor.settings.privatet.twilio.authToken;
+let client = new twilio(accountSid, authToken);
+
 Meteor.methods({
   'createNewDate'() {
     return Dates.insert({});
@@ -27,6 +33,45 @@ Meteor.methods({
         lng: lng,
         dateName: dateName,
         dateURL: dateURL
+      }
+    });
+  },
+  'autoStartDate'(id) {
+    Dates.update(id, { $set: { active: true } }, { validate: false }, function(error, response) {
+      if ( error ) {
+        console.log(error);
+      } else {
+        Notifications.insert({
+          dateId: id,
+          timestamp: new Date().getTime(),
+          eventType: 'auto-start'
+        }, function(error, response) {
+          if ( error ) {
+            console.log(error);
+          } else {
+            let date = Dates.findOne(id);
+            if ( date ) {
+              SSR.compileTemplate('auto-start-message', Assets.getText('auto-start-text.html'));
+              let t_user = Meteor.users.findOne(date.user);
+              let data = {
+                userName: t_user.profile.name,
+                address: date.address
+              };
+              dates.followers.forEach(function(follower) {
+                if ( follower.phoneNumber ) {
+                  client.messages.create({
+                    body: SSR.render('auto-start-message', data),
+                    to: '+1'+follower.phoneNumber,
+                    from: '+15097923432'
+                  })
+                  .then((message) => console.log(message.sid));
+                }
+              });
+            } else {
+              //can't find a date by id
+            }
+          }
+        });
       }
     });
   },
--- server/cron.js
+++ server/cron.js
@@ -1,5 +1,21 @@
+import { Meteor } from 'meteor/meteor';
+import { Dates } from '../imports/api/dates/dates.js';
+
 Meteor.startup(function() {
   SyncedCron.start();
+});
+
+SyncedCron.add({
+  name: "Any jobs need auto start?",
+  schedule: function(parser) {
+    return parser.text('every 1 minute');
+  },
+  job: function() {
+    let dates = Dates.find({active: false, starting: { $lte: new Date() } } ).fetch();
+    dates.forEach(function(date) {
+      Meteor.call('autoStartDate', date._id);
+    });
+  }
 });
 
 SyncedCron.add({
