54f5e7e9f6d72249987fbd85bd1bb0dbc1242e13
Kevin Korte <kevinkorte15@gmail.com>
2017-11-13 13:01:30 -0800

    changed how google maps api is loaded

--- imports/startup/client/maps.js
+++ imports/startup/client/maps.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(function() {
-  GoogleMaps.load({ v: '3', key: 'AIzaSyAL4C6JIE27NngI61BBqezolT87da5oneU', libraries: 'geometry,places' });
-})
+// Meteor.startup(function() {
+//   GoogleMaps.load({ v: '3', key: 'AIzaSyAL4C6JIE27NngI61BBqezolT87da5oneU', libraries: 'geometry,places' });
+// })
