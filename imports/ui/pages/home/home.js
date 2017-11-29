437eb729f177b07d60ef670be90d8f2b42a5da6b
kevinkorte <kevinkorte15@gmail.com>
2017-11-28 21:20:54 -0800

    working on splash page

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -3,3 +3,5 @@ import './home.html';
 import '../../components/hello/hello.js';
 import '../../components/info/info.js';
 import '../../components/menubar/menubar.js';
+import '../../components/menubar/public.js';
+import '../../components/hero/hero.js';
