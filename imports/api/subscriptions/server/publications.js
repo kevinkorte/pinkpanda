1ebe489115289a56116a2c132eb4827c78a2c6c0
kevinkorte <kevinkorte15@gmail.com>
2017-12-18 21:45:30 -0800

    cleaning up pub

--- imports/api/subscriptions/server/publications.js
+++ imports/api/subscriptions/server/publications.js
@@ -3,7 +3,5 @@ import { Subscriptions } from '../subscriptions.js';
 
 Meteor.publish('subs.all', function() {
   let user = Meteor.users.findOne(this.userId);
-  let cusId = user.stripeCustomerId;
-  console.log(Subscriptions.findOne({}));
   return Subscriptions.find({ 'resultOfStripeCreateCustomer.customer': user.stripeCustomerId });
 })
