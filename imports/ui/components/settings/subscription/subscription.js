977078d88fa8583e40184e9082df5924bd0748bf
kevinkorte <kevinkorte15@gmail.com>
2017-12-19 21:02:10 -0800

    push

--- imports/ui/components/settings/subscription/subscription.js
+++ imports/ui/components/settings/subscription/subscription.js
@@ -1,5 +1,40 @@
-
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { Subscriptions } from '../../../../api/subscriptions/subscriptions.js';
 
 import './subscription.html';
 
 import './subscription_data.js';
+
+Template.subscriptionOverview.helpers({
+  trialing() {
+    let sub = Subscriptions.findOne();
+    if ( sub ) {
+      if ( sub.resultOfStripeCreateCustomer.status == "trialing" ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  active() {
+    let sub = Subscriptions.findOne();
+    if ( sub ) {
+      if ( sub.resultOfStripeCreateCustomer.status == "active" ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  past_due() {
+    let sub = Subscriptions.findOne();
+    if ( sub ) {
+      if ( sub.resultOfStripeCreateCustomer.status == "past_due" ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+})
