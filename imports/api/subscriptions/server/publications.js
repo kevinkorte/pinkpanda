6e6719ec8549e78aa6a965d35b29d383fe718ea9
kevinkorte <kevinkorte15@gmail.com>
2018-02-20 06:28:27 -0800

    push

--- imports/api/subscriptions/server/publications.js
+++ imports/api/subscriptions/server/publications.js
@@ -3,5 +3,9 @@ import { Subscriptions } from '../subscriptions.js';
 
 Meteor.publish('subs.all', function() {
   let user = Meteor.users.findOne(this.userId);
-  return Subscriptions.find({ 'resultOfStripeCreateCustomer.customer': user.stripeCustomerId });
-})
+  if ( user ) {
+    return Subscriptions.find({ 'resultOfStripeCreateCustomer.customer': user.stripeCustomerId });
+  } else {
+    return;
+  }
+});
