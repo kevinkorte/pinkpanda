d363ca8c745f8a6848bf69bb487c08e7aa6fd962
kevinkorte <kevinkorte15@gmail.com>
2018-05-05 22:34:57 -0700

    updating email templates

--- imports/startup/server/emailTemplates.js
+++ imports/startup/server/emailTemplates.js
@@ -1,9 +1,19 @@
 import { Accounts } from 'meteor/accounts-base';
 
-Accounts.emailTemplates.from = 'TAP <tap@tap.com>';
+Accounts.emailTemplates.siteName = "SafeTAP";
+Accounts.emailTemplates.from = 'SafeTAP <safetap@safetap.net>';
 
 Accounts.emailTemplates.resetPassword.text = (user, url) => {
   return 'Hello ' + user.profile.name.first + '\n\n'
   + 'To reset your password, simply click the link below. \n\n'
   + url 
+}
+
+Accounts.emailTemplates.verifyEmail = {
+  subject() {
+    return 'Activate your account!'
+  },
+  text(user, url) {
+    return `Hey ${user.profile.name.first}! Verify your e-mail by following this link: ${url}`;
+  }
 }
